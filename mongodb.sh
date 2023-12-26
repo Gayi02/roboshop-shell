4d8e72060699d1cb1a9bef5b68f9dc25e35235f5
Gayi02 <you@example.com>
2023-12-26 12:38:00 +0530

    roboshop shell

---
+++ mongodb.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+       echo -e "Error: $2... $R failed $N"
+       exit 1
+    else
+       echo -e "$2... $G success $N"
+    fi   
+}
+
+if [ $ID -ne 0 ]
+then
+   echo -e "$R Error: Please run this script with root access $N"
+   exit 1
+else
+   echo "you are a root user"
+fi
+
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+VALIDATE $?
